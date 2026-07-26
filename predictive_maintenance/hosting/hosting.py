cb3532bc8da3f40c509c1d6172009e8af5d79e77
svebala <bala.aiml.learning@gmail.com>
2026-07-26 13:31:45 +0000

    Update predictive_maintenance

---
+++ predictive_maintenance/hosting/hosting.py
@@ -0,0 +1,25 @@
+
+from huggingface_hub import HfApi
+import os
+
+api = HfApi(token=os.getenv("HF_TOKEN"))
+
+# Upload to Hugging Face
+repo_id="BalaSVenkat/predictive-maintenance-space"
+repo_type="space"
+
+# Check if the space exists
+try:
+    api.repo_info(repo_id=repo_id, repo_type=repo_type)
+    print(f"Space '{repo_id}' already exists. Using it.")
+except RepositoryNotFoundError:
+    print(f"Space '{repo_id}' not found. Creating new space...")
+    create_repo(repo_id=repo_id, repo_type=repo_type, private=False)
+    print(f"Space '{repo_id}' created.")
+
+api.upload_folder(
+    folder_path="predictive_maintenance/deployment",     # the local folder containing your files
+    repo_id=repo_id,                                     # the target repo
+    repo_type=repo_type,                                 # dataset, model, or space
+    path_in_repo="",                                     # optional: subfolder path inside the repo
+)
